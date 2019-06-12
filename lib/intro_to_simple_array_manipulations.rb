ecd82dcc314efc2f0dbb7a384e7fb77eb6cc4f1b
Phyllis Kan <pyk9q7@gmail.com>
2019-06-12 02:31:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,3 +17,19 @@ end
 def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones.pop(2)
 end
+
+def using_shift(my_favorite_cities)
+  my_favorite_cities.shift
+end
+
+def 
+
+end
+
+def 
+
+end
+
+def 
+
+end
