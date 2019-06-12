c7a465239f31f7f8a727b72cc8c6d8896601314b
Phyllis Kan <pyk9q7@gmail.com>
2019-06-12 01:47:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,3 @@
 
 countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-updated_array = countries_in_western_africa.push("Niger")
+countries_in_western_africa.push("Niger")
