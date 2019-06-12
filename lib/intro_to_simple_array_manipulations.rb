f153b81d0f50166f0ff72157d51343e9fa38cbb3
Phyllis Kan <pyk9q7@gmail.com>
2019-06-12 01:53:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,11 @@
 def using_push(array, next_country)
-array = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-next_country = "Niger"
-using_push = array.push("Niger")
+  array = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  next_country = "Niger"
+  using_push = array.push(next_country)
+end
+
+def using_unshift(array, new_neighborhood)
+  array = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  new_neighborhood = "Brooklyn Heights"
+  using_unshift = array.unshift(new_neighborhood)
 end
