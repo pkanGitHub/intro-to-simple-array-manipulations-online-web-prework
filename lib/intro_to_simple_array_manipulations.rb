26203ff86b9f63c81a9c926a3495c72607ecc992
Phyllis Kan <pyk9q7@gmail.com>
2019-06-12 02:37:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,8 +18,8 @@ def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones.pop(2)
 end
 
-def using_shift(my_favorite_cities)
-  my_favorite_cities.shift
+def using_shift(array)
+  using_shift = array.shift
 end
 
 def
