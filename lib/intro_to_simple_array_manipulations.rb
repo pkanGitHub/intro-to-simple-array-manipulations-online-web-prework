46fb8c3a60744e31920792e98d56be3aad872bf6
Phyllis Kan <pyk9q7@gmail.com>
2019-06-12 02:38:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,8 +10,8 @@ def using_unshift(array, new_neighborhood)
   using_unshift = array.unshift(new_neighborhood)
 end
 
-def using_pop(great_hits_of_the_nineties)
-  great_hits_of_the_nineties.pop
+def using_pop(array)
+  array.pop
 end
 
 def pop_with_args(array)
